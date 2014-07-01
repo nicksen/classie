72e40af8a587ba4bedd3fb99dbf9b79ad4801e7f
Nicklas Moberg <nicklas@nicks.se>
2014-07-02 00:38:48 +0200

    Converted tabs to spaces

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,36 +1,36 @@
 var gulp = require("gulp"),
-	gutil = require("gulp-util"),
-	rimraf = require("gulp-rimraf"),
-	jshint = require("gulp-jshint"),
-	wrap = require("gulp-wrap-umd"),
-	uglify = require("gulp-uglify"),
-	rename = require("gulp-rename");
+    gutil = require("gulp-util"),
+    rimraf = require("gulp-rimraf"),
+    jshint = require("gulp-jshint"),
+    wrap = require("gulp-wrap-umd"),
+    uglify = require("gulp-uglify"),
+    rename = require("gulp-rename");
 
 gulp.task("clean", function () {
-	return gulp.src("./classie.{js,min.js}", { read: false })
-		.pipe(rimraf());
+    return gulp.src("./classie.{js,min.js}", { read: false })
+        .pipe(rimraf());
 });
 
 gulp.task("build", function () {
-	return gulp.src("./src/classie.js")
-		.pipe(jshint())
-		.pipe(jshint.reporter("jshint-stylish"))
-		.pipe(wrap({
-			exports: "classie",
-			namespace: "classie"
-		}))
-		.pipe(gulp.dest("./"));
+    return gulp.src("./src/classie.js")
+        .pipe(jshint())
+        .pipe(jshint.reporter("jshint-stylish"))
+        .pipe(wrap({
+            exports: "classie",
+            namespace: "classie"
+        }))
+        .pipe(gulp.dest("./"));
 });
 
 gulp.task("compress", [ "build" ], function () {
-	return gulp.src("./classie.js")
-		.pipe(uglify())
-		.pipe(rename("classie.min.js"))
-		.pipe(gulp.dest("./"));
+    return gulp.src("./classie.js")
+        .pipe(uglify())
+        .pipe(rename("classie.min.js"))
+        .pipe(gulp.dest("./"));
 });
 
 gulp.task("watch", function () {
-	gulp.watch("./src/classie.js", [ "build" ]);
+    gulp.watch("./src/classie.js", [ "build" ]);
 });
 
 gulp.task("default", [ "clean", "build", "watch" ]);
